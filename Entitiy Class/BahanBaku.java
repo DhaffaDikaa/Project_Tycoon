515eb07a527c89048b4bd3529f00aac7c7440a40
nnub <mnaufaljamal@gmail.com>
2026-05-07 00:02:43 +0700

    kelas bahan baku

---
+++ Entitiy Class/BahanBaku.java
@@ -0,0 +1,14 @@
+public class BahanBaku{
+    int harga;
+    String nama;
+
+    public BahanBaku(int harga, String nama){
+        this.harga = harga;
+        this.nama = nama;
+    }
+
+    public int beliBahan(){
+        //restoran
+        return 1;
+    }
+}
