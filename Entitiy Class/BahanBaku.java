f04024f3c1e05dfc4b6f1500ce20394ed1f2c91f
nnub <mnaufaljamal@gmail.com>
2026-05-07 11:09:57 +0700

    perlu diuji

--- Entitiy Class/BahanBaku.java
+++ Entitiy Class/BahanBaku.java
@@ -1,14 +1,43 @@
-public class BahanBaku{
-    int harga;
-    String nama;
+import java.util.Map;
+public class BahanBaku {
+    private int harga;
+    private String nama;
 
-    public BahanBaku(int harga, String nama){
+    public BahanBaku(int harga, String nama) {
         this.harga = harga;
         this.nama = nama;
     }
 
-    public int beliBahan(){
-        //restoran
-        return 1;
+    public String getNama() {
+        return nama;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void beliBahan(Restoran e, BahanBaku bahan, Integer jumlah) {
+        e.stok.merge(bahan, jumlah, Integer::sum);
+    }
+
+    public String kurangiStok(Restoran e, Map<BahanBaku, Integer> diPerlukan) {
+        //map"stok" akan di ambil dari atribut restoran
+        for (Map.Entry<BahanBaku, Integer> entry : diPerlukan.entrySet()) {
+            BahanBaku keperluan = entry.getKey();
+            Integer jumlahDiperlukan = entry.getValue();
+
+            //menyesuaikan Map 'diPerlukan' dan Map 'stok'
+            if (e.stok.containsKey(keperluan)) {
+                int stokSekarang = e.stok.get(keperluan);
+
+                if (stokSekarang >= jumlahDiperlukan) {
+                    e.stok.put(keperluan, stokSekarang - jumlahDiperlukan);
+                } else {
+                    return "Bahan di restoran habis :(";
+                }
+                return "Bahan tidak ada";
+            }//tidak perlu else karena barang yang di cari hanya dari pilihan GUI
+        }
+        return "";
     }
 }
--- Entitiy Class/Restoran.java
+++ Entitiy Class/Restoran.java
@@ -1,4 +1,5 @@
+import java.util.Map;
 
 public class Restoran {
-    
+    protected Map<BahanBaku, Integer> stok;
 }
