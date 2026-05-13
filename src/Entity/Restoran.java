4b1501c9ba20d68b914315ca6320ee228a81b559
nnub <mnaufaljamal@gmail.com>
2026-05-13 13:26:49 +0700

    new method tikusDatang().

--- src/Entity/Restoran.java
+++ src/Entity/Restoran.java
@@ -1,6 +1,9 @@
+
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class Restoran {
     private int kapasitas;
@@ -60,7 +63,10 @@ public class Restoran {
                 throw new BahanBakuKosongException("Menu nya ga ada nih kak");
             }
         }
-        uang += m.getHargaJual();
+
+        //transaksi(m.getHargaJual());
+        //opsi lain, jika pelanggan berstatus kabur maka, dapat di setting pada game engine nya
+        //apakah transaksi(nilai) or transaksi(0)"kabur"
     }
 
     //Menu
@@ -70,6 +76,10 @@ public class Restoran {
 
     public void hapusMenu(Menu e){
         menu.remove(e);
+    }
+
+    public Map<Menu, Integer> getMenu() {
+        return menu;
     }
 
     public Integer hitungStokMenu(Menu e) {
@@ -87,5 +97,40 @@ public class Restoran {
             bisa = Math.min(bisa, jumlahStok / jumlahDibutuhkan);
         }
         return bisa == Integer.MAX_VALUE ? 0 : bisa;
+    }
+
+    //transaksi uang agar fleksibel, ketika pelanggan kabur atau bayar.
+
+    public void transaksi(int a){
+        uang += a;
+    }
+
+    //curistok (kurangin bahan baku)
+    public void tikusDatang(){
+        Random ran = new Random();
+
+        //jumlah yang di curi 1-3 bahan maksimal dan hanya bisa curi 1 jenis stok.
+        int jumlahDiCuri = ran.nextInt(3)+1;
+
+        //barang yang mana yang akan di curi ?
+        int yangDiCuri = ran.nextInt(stok.size());
+
+        //mencari lokasi bahan.
+        BahanBaku[] rak = new BahanBaku[stok.size()];
+        rak = stok.keySet().toArray(new BahanBaku[0]);
+
+        //di curi
+        BahanBaku diCuri = rak[yangDiCuri];
+        int stokSekarang = stok.get(diCuri);
+
+        if(stokSekarang < jumlahDiCuri){
+            stokSekarang = 0;
+        }else{
+            stokSekarang -= jumlahDiCuri;
+        }
+
+        //Mengurangi
+        stok.put(diCuri, stokSekarang);
+
     }
 }
